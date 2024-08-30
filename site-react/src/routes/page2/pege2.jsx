cf8342d280d9849b3d756f92076c6d901bcf8d42
Marcusvn2006 <zubuff@gmail.com>
2024-08-30 11:09:57 -0300

    termino da page 2 page 4 e page 5/ mvn

--- site-react/src/routes/page2/pege2.jsx
+++ site-react/src/routes/page2/pege2.jsx
@@ -1,13 +1,24 @@
-
+import "./page2.css";
+import logo from "../../../public/img/logo.png"
+import { Link } from "react-router-dom";
 const Pege2 = () => {
   return (
-    <div>
-      <h1>hello world, novamente</h1>
-      <h2>Hello world</h2>
-      <h2>Hello world</h2>
-      <h2>Hello world</h2>
-    </div>
-  )
-}
+    <>
+      {" "}
+      <div className="conti">
+        <div  className="text">
+          <h2>Transforme cada<br/> gota de suor em<br/> progresso. Vá<br/> além na academia <br/>com o Gym Log e <br/>alcance seu<br/> melhor a cada<br/> treino</h2>
+        </div>
+        <div  className="img">
+          <img src="https://placehold.co/487x585" alt="" />
+        </div>
+        <div className="log">
+          <img src={logo} alt="" />
+      <Link to={"/infoAi"}><button className="btn">   <i class="bi bi-chevron-right"></i></button></Link>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Pege2
+export default Pege2;
