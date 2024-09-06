4589b2b6d0bdf504205ef89bf55ef4e0af32d94b
Marcusvn2006 <zubuff@gmail.com>
2024-09-06 16:57:43 -0300

    .

--- GymLogAPI/GymLog/obj/Debug/net8.0/GymLog.AssemblyInfo.cs
+++ GymLogAPI/GymLog/obj/Debug/net8.0/GymLog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymLog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30098fe9b986adf415bc08beb4a1731a7317ac8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d5914c0bb4cb1bddb22a1b637a373ca6ba89439")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymLog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymLog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
