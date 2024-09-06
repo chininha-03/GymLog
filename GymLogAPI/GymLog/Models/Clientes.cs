00bdb84ff9b742ec414ba8250e00b20feaa7bc0e
chininha-03 <tatazinhoow@gmail.com>
2024-09-06 08:56:53 -0300

    Backend

---
+++ GymLogAPI/GymLog/Models/Clientes.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GymLog.Models
+{
+    public class Clientes
+    {
+        public Guid ClienteId { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
+        [Display(Name = "Nome")]
+        public string ClienteName { get; set; }
+        [Required(ErrorMessage = "O campo Data de Nascimento é obrigatório.")]
+        [Display(Name = "Data de Nascimento")]
+        public DateOnly DataNasciemnto { get; set; }
+
+        [Display(Name = "Data de cadastro")]
+        public DateOnly DataCadastro { get; set; }
+
+        [Required(ErrorMessage = "O campo Sexo é obrigatório.")]
+        [Display(Name = "Sexo")]
+        public string Sexo { get; set; }
+
+        [Required(ErrorMessage = "O campo Peso é obrigatório.")]
+        [Display(Name = "Peso (KG)")]
+        public int  Peso { get; set; }
+
+        [Required(ErrorMessage = "O campo Nível de Atividade é obrigatório.")]
+        [Display(Name = "Nível de Atividade")]
+        public string NivelAtividade { get; set; }
+
+        [Required(ErrorMessage = "O campo Email é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O campo Email é inválido")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+    }
+
+
+}
---
+++ GymLogAPI/GymLog/Models/Sets.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection.PortableExecutable;
+
+namespace GymLog.Models
+{
+    public class Sets
+    {
+        public Guid SetsId { get; set; }
+        public int reps { get; set; }
+        public int peso { get; set; }
+        public string setType { get; set; }
+    }
+}
