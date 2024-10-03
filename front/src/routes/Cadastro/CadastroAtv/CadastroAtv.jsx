eab4ecd8367586f2c375e5dfddd110ddee274290
chininha-03 <tatazinhoow@gmail.com>
2024-10-03 16:21:32 -0300

    Criação da página Cadastro ativo,  falta a finalização da responsividade // chines

--- front/src/routes/Cadastro/CadastroAtv/CadastroAtv.jsx
+++ front/src/routes/Cadastro/CadastroAtv/CadastroAtv.jsx
@@ -5,30 +5,35 @@ const CadastroAtv = () => {
   return (
     <>
       <div className="cadAtv-master">
-        <div className="cadAtv-text">
-          <h3>Qual é o seu sexo biologico?</h3>
+        <div className="cadAtv-text ">
+          <h3 className="">Qual é o seu sexo biologico?</h3>
           <h5>
             Seu sexo biologico determmina as necessecidades energeticas do seu
             corpo, <br />
-            sua resposta sera utilizada para realizar o calculo da taxa
+            sua resposta será utilizada para realizar o calculo da taxa
             metabolica basal{" "}
           </h5>
         </div>
-        {/* <div className="cadAtv-escolhaSexo">
-          <button>
-            <a href="#">
-              <h3>Masculino</h3>
-              <i class="bi bi-gender-male"></i>
-            </a>
-          </button>
-          <button>
-            <a href="#">
-              <h3>Feminino</h3>
-              <i class="bi bi-gender-female"></i>
-            </a>
-          </button> */}
-
         <div className="cadAtv-escolhaSexo">
+          <div className="sex rotate-vert-center">
+            <button className="sexMasc">
+              <a href="#">
+                <i  class="bi bi-gender-male"></i>
+                <h3>Masculino</h3>
+              </a>
+            </button>
+          </div>
+          <div className="sex rotate-vert-center-2">
+            <button className="sexFem">
+              <a href="#">
+                <i class="bi bi-gender-female"></i>
+                <h3>Feminino</h3>
+              </a>
+            </button>
+          </div>
+        </div>
+
+        {/* <div className="cadAtv-escolhaSexo">
           <a href="#">
             <div class="card">
               <div class="card-overlay"></div>
@@ -48,7 +53,7 @@ const CadastroAtv = () => {
               </div>
             </div>
           </a>
-        </div>
+        </div> */}
       </div>
     </>
   );
