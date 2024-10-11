ad025ddd729415a7d2e9733f5392f93f3e434aba
chininha-03 <tatazinhoow@gmail.com>
2024-10-11 17:01:31 -0300

    Criação da página de perfil, falta finalizá-la

--- front/src/components/FotoPerfil/FotoPerfil.jsx
+++ front/src/components/FotoPerfil/FotoPerfil.jsx
@@ -1,17 +1,106 @@
-import React, { useState } from "react";
-import './FotoPerfil.css'
+import React, { useRef, useState } from "react";
+import "./FotoPerfil.css";
 
-function FotoPerfil() {
-  const [imagem, setImagem] = useState(null);
+const PhotoCircle = () => {
+  const [photo, setPhoto] = React.useState(null);
+  const fileInputRef = React.useRef(null);
 
-  const handleImageChange = (event) => {
-    setImagem(event.target.files[0]);
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setPhoto(e.target.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleButtonClick = () => {
+    fileInputRef.current.click();
   };
 
   return (
-    <div className="fotoPerfil">
-      <input type="file" onChange={handleImageChange} placeholder="Adicionar"/>
-      {imagem && <img src={URL.createObjectURL(imagem)} alt="Foto de perfil" />}
+    <div>
+      <div className="photo-container">
+        <img
+          src={
+            photo ||
+            "https://via.placeholder.com/300x300.png?text=Sua+Foto+Aqui"
+          }
+          alt="Foto do usuário"
+          className="user-photo"
+        />
+        <input
+          type="file"
+          ref={fileInputRef}
+          onChange={handleFileChange}
+          accept="image/*"
+          className="fileInput"
+        />
+        <button onClick={handleButtonClick} className="upload-btn">
+          Escolher Foto
+        </button>
+      </div>
+    </div>
+  );
+};
+
+// const PhotoCircle = () => {
+//   const [photo, setPhoto] = useState(null);
+//   const fileInputRef = useRef(null);
+
+//   const handleFileChange = (event) => {
+//     const file = event.target.files[0];
+//     if (file) {
+//       const reader = new FileReader();   
+
+//       reader.onload = (e) => {
+//         setPhoto(e.target.result);   
+
+//       };
+//       reader.readAsDataURL(file);
+//     }
+
+//     // Limpa o input após a seleção do arquivo
+//     if (fileInputRef.current) {
+//       fileInputRef.current.value = '';
+//     }
+//   };
+
+//   return (
+//     <div className="photo-circle-container">
+//       {photo ? (
+//         <img src={photo} alt="Foto de perfil" className="profile-photo" />
+//       ) : (
+//         <label htmlFor="photo-input" className="photo-circle-label">
+//           <span>Escolher foto</span>
+//           <input
+//             type="file"
+//             id="photo-input"
+//             accept="image/*"
+//             onChange={handleFileChange}
+//             ref={fileInputRef}
+
+//           />
+//         </label>
+//       )}
+//     </div>
+//   );
+// };
+
+function FotoPerfil() {
+  // const [imagem, setImagem] = useState(null);
+
+  // const handleImageChange = (event) => {
+  //   setImagem(event.target.files[0]);
+  // };
+
+  return (
+    <div>
+      {/* <input type="file" onChange={handleImageChange} placeholder="Adic" />
+      {imagem && <img src={URL.createObjectURL(imagem)} alt="Foto de perfil" />} */}
+      <PhotoCircle />
     </div>
   );
 }
--- front/src/components/HeaderPrimario/HeaderPrimario.jsx
+++ front/src/components/HeaderPrimario/HeaderPrimario.jsx
@@ -11,55 +11,55 @@ const HeaderPrimario = () => {
         <img src={imgLog} alt="" />
         <div className="searc input-group">
           <span className="input-group-text" id="basic-addon1">
-            <i class="bi bi-search"></i>
+            <i className="bi bi-search"></i>
           </span>
           <input type="text" name="" id="" />
         </div>
         <div>
-          <Link to={"./treinos"}>
+          <Link to={"/treinos"}>
             <h2></h2>
             <button className="biti">
               {" "}
-              <i class="bi bi-house"></i>Treinos
+              <i className="bi bi-house"></i>Treinos
             </button>
           </Link>
         </div>
         <div>
-          <Link to={"./rotinas"}>
+          <Link to={"/rotinas"}>
             <button className="biti">
-              <i class="bi bi-clipboard-minus"></i>
+              <i className="bi bi-clipboard-minus"></i>
               <h2>Rotinas</h2>
             </button>
           </Link>
         </div>
         <div>
-          <Link to={"./exercicio"}>
+          <Link to={"/exercicio"}>
             <button className="biti">
-              <i class="bi bi-question-lg"></i>
+              <i className="bi bi-question-lg"></i>
               <h2>Exercícios</h2>
             </button>
           </Link>
         </div>
         <div>
-          <Link to={"./perfil"}>
+          <Link to={"/perfil"}>
             <button className="biti">
-              <i class="bi bi-person-fill"></i>
+              <i className="bi bi-person-fill"></i>
               <h2>Perfil</h2>
             </button>
           </Link>
         </div>
         <div>
-          <Link to={"./loja"}>
+          <Link to={"/loja"}>
             <button className="biti">
-              <i class="bi bi-shop"></i>
+              <i className="bi bi-shop"></i>
               <h2>Loja</h2>
             </button>
           </Link>
         </div>
         <div>
-          <Link to={"./configuracoes"}>
+          <Link to={"/configuracoes"}>
             <button className="biti">
-              <i class="bi bi-gear-fill"></i>
+              <i className="bi bi-gear-fill"></i>
               <h2>Configurações</h2>
             </button>
           </Link>
@@ -68,14 +68,14 @@ const HeaderPrimario = () => {
         <div className="saidin">
           <div className="saida">
             <i
-              class="bi bi-person-circle"
+              className="bi bi-person-circle"
               style={{
                 fontSize: "40px",
               }}
             ></i>
             <h2>Perfil</h2>
             <i
-              class="bi bi-box-arrow-right"
+              className="bi bi-box-arrow-right"
               style={{
                 fontSize: "25px",
               }}
